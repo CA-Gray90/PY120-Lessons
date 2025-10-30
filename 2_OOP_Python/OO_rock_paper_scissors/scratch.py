8aa84d08813afe3668bfefc3f8c65c047f68bb0a
Charles <cgray888@gmail.com>
2025-10-30 13:08:39 +1100

    Began work in scratch file to figure out winning logic with lizard spock

--- 2_OOP_Python/OO_rock_paper_scissors/scratch.py
+++ 2_OOP_Python/OO_rock_paper_scissors/scratch.py
@@ -1,48 +1,23 @@
-class DisplayMixin:
-    def display(self):
-        print('*' * 10)
-        print(f'I am {self.name}, and I am a {self.__class__.__name__}.')
-        print(f'I am {self.age} years old.')
-        print('*' * 10)
+import random
 
-class Animal(DisplayMixin):
-    def __init__(self, name, age):
-        self._name = name
-        self._age = age
+rules = {
+    'rock' : ['scissors', 'lizard'],
+    'paper' : ['rock', 'spock'],
+    'scissors' : ['lizard', 'paper'],
+    'lizard' : ['spock', 'paper'],
+    'spock' : ['rock', 'scissors']
+}
 
-    @property
-    def name(self):
-        return self._name
-    
-    @property
-    def age(self):
-        return self._age
-    
-    @age.setter
-    def age(self, age):
-        self._age = age
+CHOICES = ('rock', 'paper', 'scissors', 'lizard', 'spock')
 
-class Dog(Animal):
-    def __init__(self, name, age):
-        super().__init__(name, age)
+player = input('Enter move: ')
+computer = random.choice(CHOICES)
 
-class Cat(Animal):
-    def __init__(self, name, age):
-        super().__init__(name, age)
-
-class Bird(Animal):
-    def __init__(self, name, age):
-        super().__init__(name, age)
-
-dog = Dog('Teddy', 3)
-cat = Cat('Fluffy', 5)
-bird = Bird('Squawky', 20)
-
-dog.display()
-dog.age = 10
-dog.display()
-
-cat.display()
-bird.display()
-bird.age = 40
-bird.display()
+print(f'Your move: {player}')
+print(f'Computer move: {computer}')
+if computer in rules[player]:
+    print('You win')
+elif player in rules[computer]:
+    print('You lose')
+else:
+    print('Its a tie')
